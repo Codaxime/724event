160edc46936366fccff64ea6024f156d06190283
Codaxime <maxime.petra@wanadoo.fr>
2025-05-29 16:53:53 +0200

    Update contact-api.cy.js

--- cypress/e2e/contact-api.cy.js
+++ cypress/e2e/contact-api.cy.js
@@ -1,12 +1,18 @@
-const apiContact =  `${Cypress.env("apiUrl")}/contact`;
-context('contact page', () => {
-    it("execute contact ",()=>{
-      cy.request("POST", apiContact, ).then((response)=>{
-        
-        expect(response.status).to.eq(200)
-        expect(response.body.message).to.contain('Merci de nous avoir contacté !')
-      })
-      
-    
-    })
-})
+describe('Test API - Formulaire de contact', () => {
+  it('POST - Envoyer un formulaire valide', () => {
+    cy.request({
+      method: 'POST',
+      url: 'http://localhost:8080/api/contact/',
+      body: {
+        name: 'Doe',
+        surname: 'Jhon',
+        contactType: 'Personnel',
+        email: 'johndoe@test.tld',
+        message: 'Exemple de message'
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(201); // ou 200
+      expect(response.body).to.have.property('message', 'Merci de nous avoir contacté !');
+    });
+  });
+});
